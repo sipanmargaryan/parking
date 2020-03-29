82c12ee62af6c57140cdc76fd31a8fb400e64910
Sipan Margaryan <sipanm19@gmail.com>
2020-03-29 20:37:10 +0400

    add translations

--- project/settings/local.py
+++ project/settings/local.py
@@ -47,6 +47,7 @@ INSTALLED_APPS.extend(EXTERNAL_APPS + PROJECT_APPS)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+ugettext = lambda s: s  # noqa
+LANGUAGES = (
+    ('en', ugettext('English')),
+    ('am', ugettext('Armenian')),
+)
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -146,7 +157,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
