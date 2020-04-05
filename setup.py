a8af1d7c876b7622bcba04ffba8baff2e8db89f0
Sipan Margaryan <sipanm19@gmail.com>
2020-04-05 22:24:11 +0400

    setup

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name='parking',
     version='0.1',
-    package_dir={'': 'apps', 'project': './project'},
-    packages=find_packages('apps') + ['project', 'apps/core'],
+    # package_dir={'': 'apps', 'project': './project'},
+    packages=find_packages('apps') + ['project'],
 )
